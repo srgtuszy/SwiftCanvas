20e83297dd06467959f51f57f4f614d20a29d5d9
Michał Tuszyński <srgtuszy@gmail.com>
2018-02-18 22:53:25 +0100

    Initial Commit

---
+++ Canvas/Canvas.h
@@ -0,0 +1,19 @@
+//
+//  Canvas.h
+//  Canvas
+//
+//  Created by Michal Tuszynski on 18/02/2018.
+//  Copyright © 2018 iapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Canvas.
+FOUNDATION_EXPORT double CanvasVersionNumber;
+
+//! Project version string for Canvas.
+FOUNDATION_EXPORT const unsigned char CanvasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Canvas/PublicHeader.h>
+
+
